fe2198950cb6019f0feb5d6055d3c8e5564b9af8
rr4tune7 <rr4tune@gmail.com>
2026-02-04 18:21:37 +0400

    Add files via upload

--- search_excel_app.py
+++ search_excel_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="Поиск по Excel", layout="wide")
-st.title("🔍 KMK 30.01.2026" )
+st.title("🔍 KMK")
 
 # Ссылка на Excel
-url = "https://www.dropbox.com/scl/fi/kgba4pibh4nqzt9fo42l3/.xlsx?rlkey=2qs2s9uemmz5o8a0mqax5eahj&st=5sv0qixv&dl=1"
+url = "https://www.dropbox.com/scl/fi/8ncsz4wpl94owruvmv4l2/.xlsx?rlkey=hhmc41roywrr5qzmvor5rxlbx&st=wcpqphai&dl=1"
 
 # Загрузка Excel
 try:
@@ -21,31 +21,16 @@ st.dataframe(df)
 search = st.text_input("Введите ключевое слово для поиска в ԱՆՎԱՆՈՒՄ:")
 
 if search:
-    # 1. Поиск в АՆՎԱՆՈՒՄ
+    # 1. Поиск в ԱՆՎԱՆՈՒՄ
     df_filtered = df[df['ԱՆՎԱՆՈՒՄ'].astype(str).str.contains(search, case=False, na=False)]
 
-    # 2. Столбцы для проверки непустоты (все кроме ԱՐԺԵՔ и ՏԵՂԱԴՐՈՒՄ и ԱՆՎԱՆՈՒՄ)
-    columns_to_check = [col for col in df_filtered.columns if col not in ['ԱՐԺԵՔ', 'ՏԵՂԱԴՐՈՒՄ', 'ԱՆՎԱՆՈՒՄ']]
+    # 2. Столбцы, которые нужно проверять на непустые значения (все кроме ԱՐԺԵՔ и ՏԵՂԱԴՐՈՒՄ и АՆՎԱՆՈՒՄ)
+    columns_to_check = [col for col in df.columns if col not in ['ԱՐԺԵՔ', 'ՏԵՂԱԴՐՈՒՄ', 'ԱՆՎԱՆՈՒՄ']]
 
     # 3. Оставляем только строки, где хотя бы одно значение в этих столбцах не пустое
-    df_non_empty_rows = df_filtered[df_filtered[columns_to_check].notna().any(axis=1)]
-
-    # 4. Оставляем только столбцы, где есть хотя бы одно непустое значение
-    df_non_empty = df_non_empty_rows.loc[:, df_non_empty_rows.notna().any(axis=0)]
+    df_non_empty = df_filtered[df_filtered[columns_to_check].notna().any(axis=1)]
 
     st.subheader(f"Результаты поиска по '{search}' в ԱՆՎԱՆՈՒՄ")
     st.dataframe(df_non_empty)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
