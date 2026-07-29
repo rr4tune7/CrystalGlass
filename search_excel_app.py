b499eb78f20de80ff02ff04877b94f3be6addc32
rr4tune7 <rr4tune@gmail.com>
2026-07-29 18:38:43 +0400

    Update search_excel_app.py

--- search_excel_app.py
+++ search_excel_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import re
 
 st.set_page_config(page_title="Поиск по Excel", layout="wide")
 st.title("🔍 KMK 28.07.2026")
@@ -18,90 +17,12 @@ except Exception as e:
 st.subheader("Все данные")
 st.dataframe(df)
 
-def normalize_word(word):
-    """
-    Преобразует слово в упрощённое написание.
-    Например:
-    rogue   -> ruj
-    camry   -> camri
-    alphard -> alfard
-    """
-
-    word = str(word).lower().strip()
-
-    # Удаляем знаки и оставляем только буквы и цифры
-    word = re.sub(r"[^a-zа-яё0-9]", "", word)
-
-    # Частые варианты написания
-    word = word.replace("ph", "f")
-    word = word.replace("ck", "k")
-
-    # Rogue -> ruj
-    word = re.sub(r"ogue$", "uj", word)
-
-    # Например: range -> ranj
-    word = re.sub(r"ge$", "j", word)
-
-    # Camry -> camri
-    word = re.sub(r"y$", "i", word)
-
-    return word
-
-
-def normalize_words(value):
-    """
-    Разделяет название на слова
-    и нормализует каждое слово.
-    """
-
-    if pd.isna(value):
-        return []
-
-    text = str(value).lower()
-
-    words = re.findall(
-        r"[a-zа-яё0-9]+",
-        text
-    )
-
-    return [
-        normalize_word(word)
-        for word in words
-        if normalize_word(word)
-    ]
-
-
-def smart_exact_search(value, search_text):
-    """
-    Строгий поиск после преобразования слов.
-    Все введённые слова должны присутствовать
-    в названии товара.
-    """
-
-    excel_words = normalize_words(value)
-    search_words = normalize_words(search_text)
-
-    if not excel_words or not search_words:
-        return False
-
-    return all(
-        search_word in excel_words
-        for search_word in search_words
-    )
-
 # Ввод ключевого слова
 search = st.text_input("Введите ключевое слово для поиска в ԱՆՎԱՆՈՒՄ:")
 
 if search:
     # 1. Поиск в ԱՆՎԱՆՈՒՄ
-   df_filtered = df[
-    df["ԱՆՎԱՆՈՒՄ"].apply(
-        lambda value: smart_exact_search(
-            value,
-            search
-        )
-    )
-]
+    df_filtered = df[df['ԱՆՎԱՆՈՒՄ'].astype(str).str.contains(search, case=False, na=False)]
 
     # 2. Столбцы, которые нужно проверять на непустые значения (все кроме ԱՐԺԵՔ и ՏԵՂԱԴՐՈՒՄ и АՆՎԱՆՈՒՄ)
     columns_to_check = [col for col in df.columns if col not in ['ԱՐԺԵՔ', 'ՏԵՂԱԴՐՈՒՄ', 'ԱՆՎԱՆՈՒՄ']]
@@ -111,8 +32,3 @@ if search:
 
     st.subheader(f"Результаты поиска по '{search}' в ԱՆՎԱՆՈՒՄ")
     st.dataframe(df_non_empty)
-
-
-
-
-
